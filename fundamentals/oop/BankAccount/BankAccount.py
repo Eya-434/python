128dcb59e4afef301f6dd152dc09ceb2afb6ac83
Eya-434 <eyazarrad2@gmail.com>
2024-11-27 13:25:22 +0100

    hi

--- fundamentals/oop/BankAccount/BankAccount.py
+++ fundamentals/oop/BankAccount/BankAccount.py
@@ -6,20 +6,31 @@ class BankAccount:
         if amount>0 :
             self.balance+=amount
             print(f"Balance :{self.balance}")
-        else: print("")
     def withdraw(self, amount):
         if amount>0 and amount<= self.balance :
             self.balance-=amount
             print(f"Balance :{self.balance}")
-        else: print("Insufficient funds: Charging a $5 fee")
-        self.balance-=5
+        else:
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance -= 5
+            print(f"Balace :{self.balance}")
     def display_account_info(self):
         print(f"Balance:{self.balance}")
     def yield_interest(self):
         if self.balance>0:
-            interest = self.balance*self.int_rate
-            self.balance+=interest
+            self.balance+=(self.balance*self.int_rate)
             print(f"Balance+ Interest:{self.balance}")
-print("hello!")
+
 account1= BankAccount(0.02, 1000)
 account2= BankAccount(0.03,5000)
+
+account1.deposit(3)
+account1.withdraw(1)
+account1.yield_interest()  
+account1.display_account_info()
+
+
+account2.deposit(2)
+account2.withdraw(4)
+account2.yield_interest()
+account2.display_account_info()
